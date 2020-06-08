0456bed49a98cad1813d5487bbf7b4538cbaff2f
Nicolas Fish <nicolasfish1224@gmail.com>
2020-06-08 15:49:52 -0700

    Cinema Menu

--- Assets/Scripts/Menus/PlayGame.cs
+++ Assets/Scripts/Menus/PlayGame.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class PlayGame : MonoBehaviour
 {
+    public VideoPlayer vid;
+
+    void Start() { vid.loopPointReached += CheckOver; }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        Play();
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
